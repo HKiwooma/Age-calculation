8daceddb752aeda6696f6987656df3639498bb20
HarunaIbnYusuf <39216603+HarunaIbnYusuf@users.noreply.github.com>
2018-07-27 17:27:37 +0300

    Create age-in-seconds.py

---
+++ age-in-seconds.py
@@ -0,0 +1,5 @@
+def age_in_second():
+    user_age = input("enter you age: ")
+    age_seconds = int(user_age)*365*24*60*60
+    print("you have lived for {} seconds".format(age_seconds))
+age_in_second()
